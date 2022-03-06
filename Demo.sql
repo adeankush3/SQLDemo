25a7b6bb72e6f58200bfcd083df171e7669dc74e
adeankush3 <adeankush3@gmail.com>
2022-03-06 21:09:32 +0530

    [Ankush]SQL Demo Code

---
+++ Demo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Persons (
+	PersonID int,
+	LastName varchar(255),
+	FirstName varchar(255),
+	Address varchar(255),
+	City varchar(255),
+	Primary KEY (PersonId),
+);
+select * from Persons;
+Insert into Persons (PersonID,LastName,FirstName,Address,City) values(111,'Ankush','Ade','Walki','Nanded')
